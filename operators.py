f54d4ebb505b0029d87c29ecf232114185f02775
s_adamowski <s.adamowski94@gmail.com>
2020-06-07 23:32:57 +0200

    Implemented Operators (Remove All Materials, Add Triangulate Modifier)

--- operators.py
+++ operators.py
@@ -1,4 +1,5 @@
 import bpy
+import re
 #import logging
 
 class RemoveDublicateMaterials_Operator(bpy.types.Operator):
@@ -6,36 +7,63 @@ class RemoveDublicateMaterials_Operator(bpy.types.Operator):
     bl_label = "Remove Dublicate Materials"
     bl_description = "Remove dublicate materials from the object."
 
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects is not None
+
     def execute(sel, context):
-        mat_list = [x.material.name for x in bpy.context.object.material_slots]
-        remove_slots = []
-        
-        bpy.ops.object.mode_set(mode='OBJECT')
-
-        for s in bpy.context.object.material_slots:
-             if s.material.name[-3:].isnumeric():
-                 if s.material.name[:-4] in mat_list:
-                     index_clean = mat_list.index(s.material.name[:-4])
-                     index_wrong = mat_list.index(s.material.name)
-                     print(index_wrong, index_clean)
-                     faces = [x for x in bpy.context.object.data.polygons if x.material_index == index_wrong]
-
-                     for f in faces:
-                         f.material_index = index_clean
-                     remove_slots.append(s.name)
-        for s in remove_slots:
-            if s in [x.name for x in bpy.context.object.material_slots]:
-                print('removing slot %s' % s)
-                bpy.context.object.active_material_index = [x.material.name for x in bpy.context.object.material_slots].index(s)
-                bpy.ops.object.material_slot_remove()
-
-
+        matlist = bpy.data.materials
+        for obj in bpy.context.selected_objects:
+            for mat_slt in obj.material_slots:
+                if (mat_slt.material != None and bool(re.search(r'\.(\d){3}', mat_slt.material.name))):
+                    replaced = False
+                    for mat in matlist:
+                        if ((mat.name == mat_slt.material.name.rsplit(".", 1)[0]) and (not bool(re.search(r'\.(\d){3}', mat.name)))):
+                            mat_slt.material = mat
+                            replaced = True
+                    if replaced == False:
+                        mat_slt.material.name = mat_slt.material.name.rsplit(".", 1)[0]
         return {'FINISHED'}
-        
+
+class RemoveAllMaterials_Operator(bpy.types.Operator):
+    bl_idname = "object.remove_all_materials"
+    bl_label = "RemoveAllMaterials"
+    bl_description = "Removes all materials from selected objects"
+
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects is not None
+
+    def execute(self, context):
+        for obj in bpy.context.selected_objects:
+            obj.active_material_index = 0
+            for i in range(len(obj.material_slots)):
+                bpy.ops.object.material_slot_remove({'object': obj})
+        return {'FINISHED'}
+
+
 class AddTriangulateModifier_Operator(bpy.types.Operator):
     bl_idname = "object.add_triangulate_modifier"
     bl_label = "AddTriangulateModifier"
     bl_description = "Add Triangulate Modifier to selection objects."
+ 
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects is not None
 
     def execute(self, context):
+        
+        for obj in bpy.context.selected_objects:
+            countTriangulateModifiers = 0
+            for mod in obj.modifiers:
+                if (mod.type == "TRIANGULATE"):
+                    countTriangulateModifiers += 1
+                    mod.keep_custom_normals = True
+                    #move Modifier at the bottom of stack
+            if (countTriangulateModifiers == 0):
+                modTriangulate = obj.modifiers.new("Triangulate", type = "TRIANGULATE")
+                modTriangulate.keep_custom_normals = True
+        
+        self.report({'INFO'}, "Added triangulate modifier to selected objects")
+        bpy.context.view_layer.update()
         return {'FINISHED'}
